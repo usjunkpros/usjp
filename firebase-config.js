df5fb10be76b399391edd49928da68ac386560ab
usjunkpros <customercare.usjunkpros@gmail.com>
2026-05-24 18:38:02 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBsjuByq1OY2JmWXx2FjmnC6jyXqi7OpkE",
+  authDomain: "us-junk-pros.firebaseapp.com",
+  projectId: "us-junk-pros",
+  storageBucket: "us-junk-pros.firebasestorage.app",
+  messagingSenderId: "342886642933",
+  appId: "1:342886642933:web:fb5ab4c136834ec72b01bc",
+  measurementId: "G-JY6GPJ7Z8G"
+};
+
+export const app = initializeApp(firebaseConfig);
